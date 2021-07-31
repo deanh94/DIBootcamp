f8f6275c86e29b36e9946422c4b67a636ffc0924
deanh94 <dean.haim94@gmail.com>
2021-08-01 01:47:53 +0300

    project

---
+++ Week5/Day5/project.js
@@ -0,0 +1,44 @@
+// const loadBox = document.getElementById("vanilla")
+const monkeys = document.getElementById("box")
+const loadTxt = document.createElement("p")
+loadTxt.textContent = "hold on.."
+// loadbox.appendChild(loadTxt)
+
+const errormsg = document.createElement("p")
+errormsg.textContent = "that person isn't available" 
+
+const originalURL = "https://swapi.dev/api/people/"
+const jedi = async() => {
+    monkeys.innerHTML = ""
+    try{ 
+        monkeys.appendChild(loadBox)
+        const randomURL = originalURL+Math.floor(Math.random() * 100); // same as making var and adding to url link
+  const jediWait = await fetch(randomURL) 
+  const dataURL = await jediWait.json() //makes it js object (u can use it)
+  const homeworldURL = dataURL.homeworld
+  monkeys.removeChild(loadBox)
+  const homeW = await fetch(homeworldURL)
+  const homeS = await homeW.json()
+
+
+  const jediArr = [dataURL.name, dataURL.height, dataURL.birth_year, homeS.name, dataURL.gender]
+  console.log(jediArr);
+  jediArr.forEach((element) => { // element, index, array
+  let p = document.createElement("p")
+  p.innerHTML = element
+  monkeys.appendChild(p);
+  }) 
+    }
+
+    catch{
+        console.log("theres a problem!")
+        monkeys.appendChild(errormsg)
+        // setTimeout(() => monkeys.removeChild(errormsg),3000)
+    }
+
+    }
+
+
+
+const button = document.getElementById("myBtn")
+button.addEventListener("click", jedi);
---
+++ Week5/Day5/project2.js
@@ -0,0 +1,8 @@
+const originalURL = "http://api.currencylayer.com/live?access_key=946eabf9345d6b2f56fd326616a59adf&format=1"
+const moneys = document.getElementById("box")
+const currencies = async() => {
+    moneys.innerHTML = ""
+    try{
+
+    }
+finally}
