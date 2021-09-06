a2e2d39a8183774ceed5c2e5338f70d75f6ed4c8
deanh94 <dean.haim94@gmail.com>
2021-09-06 09:32:11 +0300

    tictactoe game

---
+++ Week10/day1/tictactoe/src/components/Square.js
@@ -0,0 +1,10 @@
+import React from 'react'  
+import PropTypes from 'prop-types'
+
+export default function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}   
